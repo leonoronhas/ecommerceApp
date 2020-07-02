660b8dc6a4d24d25cbba5da03fbe2fb672cf28ad
Leonardo <leosud099@gmail.com>
2020-07-01 19:03:10 -0600

    Folder structures and files 01

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -11,11 +11,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, Flatilist } from "react-native";
+
+const ProductsOverviewScreen = (props) => {
+  return (
+    <Flatilist
+  );
+};
+
+export default ProductsOverviewScreen;
